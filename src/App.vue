c5dcf7dadcd49f0363dfbc3cb8ae29d567eeab84
Giacomo-Arcangeli <giacomoarcangeli1997@gmail.com>
2023-06-29 16:51:52 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<script>
+import AppHeader from './components/AppHeader.vue'
+import AppMain from './components/AppMain.vue'
+import AppFooter from './components/AppFooter.vue'
+export default {
+  components: { AppHeader, AppMain, AppFooter },
+  data() {
+    return
+  }
+}
+</script>
+
+<template>
+  <div class="container">
+    <AppHeader />
+    <AppMain />
+    <AppFooter />
+  </div>
+</template>
+
+<style>
+/* reset */
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+/* generics */
+
+body {
+  background-image: url();
+  background-repeat: no-repeat;
+}
+
+li {
+  list-style-type: none;
+}
+
+img {
+  height: auto;
+  max-width: 100%;
+}
+
+a {
+  color: black;
+  text-decoration: none;
+}
+
+/* utilities */
+.container {
+  max-width: 1200px;
+  margin: 0 auto;
+}
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+    data() {
+        return
+    }
+}
+</script>
+
+<template>
+    <header>
+        <img src="../assets/img/dc-logo.png" alt="">
+        <div>
+            <ul>
+                <li><a href="#">CIAO</a></li>
+                <li><a href="#">CIAO</a></li>
+                <li><a href="#">CIAO</a></li>
+                <li><a href="#">CIAO</a></li>
+                <li><a href="#">CIAO</a></li>
+                <li><a href="#">CIAO</a></li>
+                <li><a href="#">CIAO</a></li>
+                <li><a href="#">CIAO</a></li>
+                <li><a href="#">CIAO</a></li>
+                <li><a href="#">CIAO</a></li>
+            </ul>
+        </div>
+    </header>
+</template>
+
+<style>
+header {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100px;
+    border: 2px dashed red;
+}
+</style>
